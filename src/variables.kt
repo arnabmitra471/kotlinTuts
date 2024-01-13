ad7693041564d5753de3f4a08e2c62defd617613
Arnab Mitra <arnabmitra471@gmail.com>
2024-01-13 14:15:05 +0530

    Completed the various primitive data types in kotlin

---
+++ src/variables.kt
@@ -0,0 +1,17 @@
+fun main()
+{
+    var score: Int
+    val msg = "Kotlin is Awesome"
+    score = 100
+    println("The score of the match is "+score)
+    println(msg)
+    var temp: Float
+    temp = 67.12f
+    println("The temperature of the oven is "+temp)
+    var isRunning: Boolean
+    isRunning = false
+    println(isRunning)
+    var str :String
+    str = "Music has a lot of power in itself"
+    println(str)
+}
